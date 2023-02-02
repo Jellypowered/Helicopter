fec400ceb43e7c9cbf219552b96d21ae4ecc7682
LordWabbit <lordwabbit@gmail.com>
2023-02-02 21:01:02 +0200

    fixed harmony patches

---
+++ Source/Helicopter/HarmonyBoot.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Helicopter
+{
+    [StaticConstructorOnStartup]
+    internal static class HarmonyBoots
+    {
+        static HarmonyBoots()
+        {
+            var harmony = new Harmony("jelly.helicopter");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
